796cd83aef621f52bc5d5166a3a66e399c5c6380
gu9uszzz <1021074244@qq.com>
2025-04-20 19:05:11 +0800

    Add files via upload

---
+++ backend/user.sql
@@ -0,0 +1,2 @@
+INSERT INTO `user` (`id`, `username`, `password`, `email`, `avatar`, `date`, `lastlogin`, `address`, `sign`) VALUES (13, '77777', 'Ab123456', '1021074244@qq.com', NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `user` (`id`, `username`, `password`, `email`, `avatar`, `date`, `lastlogin`, `address`, `sign`) VALUES (15, '77778', 'Ab123456', '3334074255@qq.com', '1551362', NULL, NULL, NULL, NULL);
